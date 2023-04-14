2eb8a83a5c9c095a31ea3b7bff73d0324f3621ae
nensibavisi <130463985+nensibavisi@users.noreply.github.com>
2023-04-14 17:44:53 +0530

    Add files via upload

---
+++ if else 1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+	 int a=10,b=20;
+	 if(a<b)
+     {
+     	printf("a is minimum");
+	 }
+																			                                                                      
+	else
+	{
+		printf(" b is minimum");
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+
---
+++ if else2.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+main()
+{
+	int age;
+	printf("enter your age=");
+	scanf("%d",&age);
+	
+	if(age<0)
+	{
+		printf("value is possitive");
+	
+	}
+	else if(age>0)
+	{
+		("value is nagative");
+	}
+	else if(age==0)
+	{
+		printf("value is neutral");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
